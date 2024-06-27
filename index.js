9491d54406e9c9a64235a035266000320797122c
Akhil9249 <akhklcv430@gmail.com>
2024-06-27 23:20:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const {add,sub,divide} = require("./utils")
+
+let addResult = add(10,20)
+
+console.log(addResult,"==addResult");
+
+let subResult = sub(10,20)
+
+console.log(subResult,"==subResult");
+
+let divideResult = divide(10,20)
+
+console.log(divideResult,"==divideResult")
---
+++ utils.js
@@ -0,0 +1,14 @@
+const add = (first,second)=>{
+    return first+second
+}
+
+const sub = (first,second)=>{
+    return first-second
+}
+
+const divide = (first,second)=>{
+    return first/second
+}
+
+module.exports = {add,sub,divide}
+
